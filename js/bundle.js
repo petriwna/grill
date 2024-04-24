33aef16e7f8ca7fca58078d43f719133768607fc
petriwna <petriwna43@gmail.com>
2024-04-24 20:00:17 +0300

    Update 2024-04-24T17:00:14.493Z

--- js/bundle.js
+++ js/bundle.js
@@ -1,5 +1,6 @@
 
 
+
 export class Catalog {
     constructor() {
         this.cards = document.querySelectorAll('.card');
@@ -40,7 +41,12 @@ export class Catalog {
 
 
 export function init() {
-    console.log("init");
+    document.getElementById('up').addEventListener('click', () => {
+        window.scrollTo({
+            top: 0,
+            behavior: 'smooth'
+        });
+    })
     new Catalog();
 }
 
@@ -52,6 +58,8 @@ export class Modal {
         this.modalBtnClose = document.querySelector(closeBtnSelector);
         this.modalTitle = document.querySelector('.modal__title');
         this.modalCode = document.querySelector('.modal__code');
+        this.modalPrice = document.querySelector('.modal__price');
+        this.modalDescription = document.querySelector('.modal__description')
         this.handleClickClose();
     }
 
@@ -62,15 +70,33 @@ export class Modal {
 
         if (isBackdropHidden) {
             this.getCategory(card)
-            console.log(card.dataset.id);
+        } else {
+            this.close();
         }
     }
 
     getCategory(card) {
         const cardTitle = card.querySelector('.card__title').textContent;
         const cardCode = card.querySelector('.card__code').textContent;
+        const cardPriceOld = card.querySelector('.price__old').textContent;
+        const cardPriceNew = card.querySelector('.price__new').textContent;
+        const cardDescription = card.querySelector('.product');
+        const cardDescriptionCopy = cardDescription.cloneNode(true);
+
+        cardDescriptionCopy.classList.remove('visually-hidden');
         this.modalTitle.textContent = cardTitle;
         this.modalCode.textContent = cardCode;
+        this.modalPrice.innerHTML = `
+            <p class="price__new">${cardPriceNew}</p>
+            <p class="price__old">${cardPriceOld}</p>
+        `;
+        this.modalDescription.appendChild(cardDescriptionCopy);
+    }
+
+    close() {
+        const cardDescriptionCopy = this.modalDescription.querySelector('.product');
+
+        this.modalDescription.removeChild(cardDescriptionCopy)
     }
 
     handleClickClose() {
